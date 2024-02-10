f48a16add686ca02b61e85b00e83f9f3edb02332
Niki <tofitaakk@gmail.com>
2024-02-10 20:56:05 +0200

    початковий файл

---
+++ HW1002.py
@@ -0,0 +1,51 @@
+def validate_month(month):
+    try:
+        month = int(month)
+        if month >= 1 or month <= 12:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
+def validate_year(year):
+    try:
+        year = int(year)
+        if year >= 1900 and year <= 2019:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
+def create_test_file(name, year, month):
+    with open("testFile.txt", "w") as f:
+        f.write(f"Прізвище: {name}\n")
+        f.write(f"Рік народження: {year}\n")
+        f.write(f"Місяць народження: {month}\n")
+
+
+def get_user_input():
+
+    name = input("Введіть ваше ім'я: ")
+
+    while True:
+        year = input("Введіть рік народження: ")
+        month = input("Введіть місяць народження: ")
+
+        if validate_month(month) == True and validate_year(year) == True:
+            return name, month, year
+            break
+        else:
+            print("Некоректний місяць чи рік!\n"
+                  "Будь ласка, введіть реальні дані")
+
+
+def main():
+    name, year, month = get_user_input()
+    create_test_file(name, year, month)
+    print('Програма завершенна')
+
+
+if __name__ == '__main__':
+    main()
